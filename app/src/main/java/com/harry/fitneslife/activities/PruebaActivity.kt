3b7ae8628540e2af1007b4782dd01ed17d08039a
Leonardo <leo1.25daniel@gmail.com>
2024-02-13 00:49:45 -0600

    Pruebas

---
+++ app/src/main/java/com/harry/fitneslife/activities/PruebaActivity.kt
@@ -0,0 +1,12 @@
+package com.harry.fitneslife.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.harry.fitneslife.R
+
+class PruebaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_prueba)
+    }
+}
