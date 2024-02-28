e5f06f1d7bc83d39a67d3b763ac98197e8eec73e
Leonardo <leo1.25daniel@gmail.com>
2024-02-28 02:15:41 -0600

    Haciendo merge para la traducción

--- app/src/main/java/com/harry/fitneslife/funAndClass/ListBox.kt
+++ app/src/main/java/com/harry/fitneslife/funAndClass/ListBox.kt
@@ -1,15 +1,14 @@
 package com.harry.fitneslife.funAndClass
 
-import android.provider.Settings.System.getString
 import com.harry.fitneslife.R
 
 class ListBox{
     companion object{
         val carrusel = listOf<HomeBox>(
-            HomeBox(getString(R.string.resuelva), R.drawable.ic_cardio),
-            HomeBox(getString(R.string.ejercicio),R.drawable.ic_exercise),
-            HomeBox(getString(R.string.rutina),R.drawable.ic_routine),
-            HomeBox(getString(R.string.body),R.drawable.ic_body)
+            HomeBox("getString(R.string.resuelva)", R.drawable.ic_cardio),
+            HomeBox("getString(R.string.ejercicio)",R.drawable.ic_exercise),
+            HomeBox("getString(R.string.rutina)",R.drawable.ic_routine),
+            HomeBox("getString(R.string.body)",R.drawable.ic_body)
         )
 
 
