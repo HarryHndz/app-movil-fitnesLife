654119ce15fe2540e4fdf28a177a3082621e104e
Harry Hernandez Arias <132928917+HarryHndz@users.noreply.github.com>
2024-04-10 08:02:30 -0600

    corrección de bug del cronometro

--- app/src/main/java/com/harry/fitneslife/fragments/CronometroRutinaFragment.kt
+++ app/src/main/java/com/harry/fitneslife/fragments/CronometroRutinaFragment.kt
@@ -15,6 +15,7 @@ class CronometroRutinaFragment : Fragment() {
     private val bindig get() = _binding!!
     private var elapsedTime = 0L
     private lateinit var countDownTimer: CountDownTimer
+    private var pausar = false
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -25,33 +26,40 @@ class CronometroRutinaFragment : Fragment() {
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
+
         bindig.btnCronometro.setOnClickListener {
-            bindig.cronometro.base = SystemClock.elapsedRealtime() - elapsedTime
-            bindig.cronometro.start()
 
-            bindig.desCrono.base = SystemClock.elapsedRealtime()
+            if (!pausar){
+                bindig.cronometro.base = SystemClock.elapsedRealtime() - elapsedTime
+                bindig.cronometro.start()
+                bindig.desCrono.base = SystemClock.elapsedRealtime()
+                pausar = true
+            }else{
+                bindig.cronometro.stop()
+                elapsedTime = SystemClock.elapsedRealtime() - bindig.cronometro.base
 
-        }
-        bindig.btnCronPause.setOnClickListener {
-            bindig.cronometro.stop()
-            elapsedTime = SystemClock.elapsedRealtime() - bindig.cronometro.base
+                countDownTimer = object : CountDownTimer(10000,1000){
+                    override fun onTick(millisUntilFinished: Long) {
+                        bindig.desCrono.text = (millisUntilFinished / 1000).toString()
+                        bindig.btnCronometro.isEnabled = false
+                    }
 
-            countDownTimer = object : CountDownTimer(10000,1000){
-                override fun onTick(millisUntilFinished: Long) {
-                    bindig.desCrono.text = (millisUntilFinished / 1000).toString()
+                    override fun onFinish() {
+                        bindig.desCrono.text = "¡Ha terminado el descanso!"
+                        bindig.cronometro.base = SystemClock.elapsedRealtime() - elapsedTime
+                        bindig.cronometro.start()
+                        bindig.btnCronometro.isEnabled = true
+
+                    }
+
                 }
-
-                override fun onFinish() {
-                    bindig.desCrono.text = "¡Ha terminado el descanso!"
-                }
-
+                countDownTimer.start()
+                pausar = true
             }
-            countDownTimer.start()
         }
-        bindig.btnCronFin.setOnClickListener {
-            bindig.cronometro.base = SystemClock.elapsedRealtime()
-            bindig.cronometro.stop()
-        }
+
     }
 
     override fun onDestroy() {
